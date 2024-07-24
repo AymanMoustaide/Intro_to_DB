75b0e2b88507cc2f393494132587b32113d231c8
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-24 19:21:09 +0100

    :

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,23 +1,18 @@
--- Create the database
 CREATE DATABASE alx_book_store;
 
--- Use the database
-USE alx_book_store;
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
 
 -- Create Authors table
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
-);
-
--- Create Books table
-CREATE TABLE Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 -- Create Customers table

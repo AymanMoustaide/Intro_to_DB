5d134ae494b478f73173879c838f5b147f6cee87
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-25 01:58:27 +0100

    ::

--- MySQLServer.py
+++ MySQLServer.py
@@ -9,19 +9,21 @@ try:
         password = 'Root00root',
         database = 'alx_book_store',
     )
+    
+except mysql.connector.Error as err:
+    print("Something went wrong: {}".format(err))
+
 
 
 #* EXECUTE COMMAND TO DB
-    if mydb.is_connected():
-        print('DATABASE AlX_STORE_STORE WORK SUCCESSFULLY!')
+if mydb.is_connected():
+    print('DATABASE AlX_STORE_STORE WORK SUCCESSFULLY!')
 
-    else:
-        cursor = mydb.cursor()
-        cursor.execute('CREATE DATABASE IF NOT EXISTS ALX_BOOK_STORE')
+else:
+    cursor = mydb.cursor()
+    cursor.execute('CREATE DATABASE IF NOT EXISTS ALX_BOOK_STORE')
 
 
 
-except mysql.connector.Error as err:
-    print("Something went wrong: {}".format(err))
 
 

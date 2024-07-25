0c31afb211a1a89f84baad1d14b8358cef4760fc
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-25 02:02:30 +0100

    ::

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,24 +1,32 @@
 import mysql.connector
 
 
+try:
+        
+    mydb = mysql.connector.connect(
+        host = '127.0.0.1',
+        port = '3306',
+        user = 'root',
+        password = 'Root00root',
+        database = 'alx_book_store',
+    )
+    if mydb.is_connected():
+        #* TEST THE DATABSE IS CONNECTED
+        print('DATABASE AlX_STORE_STORE WORK SUCCESSFULLY!')
+    else:
 
-mydb = mysql.connector.connect(
-    host = '127.0.0.1',
-    port = '3306',
-    user = 'root',
-    password = 'Root00root',
-    database = 'alx_book_store',
-)
+        #* EXECUTE COMMAND TO DB
+        cursor = mydb.cursor()
+        cursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
 
-#* EXECUTE COMMAND TO DB
-if mydb.is_connected():
-    print('DATABASE AlX_STORE_STORE WORK SUCCESSFULLY!')
+except mysql.connector.Error as err:
+    print("Something went wrong: {}".format(err))
 
-else:
-    cursor = mydb.cursor()
-    cursor.execute('CREATE DATABASE IF NOT EXISTS alx_book_store')
+    
+
+    
 
 
-
+    
 
 

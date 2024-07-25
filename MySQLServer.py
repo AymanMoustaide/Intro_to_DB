432bc4060cc25ad8a6427e8f9a032518fe73c769
AymanMoustaide <ayman.moustaide@icloud.com>
2024-07-25 01:59:11 +0100

    ::

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,19 +1,14 @@
 import mysql.connector
 
 
-try:
-    mydb = mysql.connector.connect(
-        host = '127.0.0.1',
-        port = '3306',
-        user = 'root',
-        password = 'Root00root',
-        database = 'alx_book_store',
-    )
-    
-except mysql.connector.Error as err:
-    print("Something went wrong: {}".format(err))
 
-
+mydb = mysql.connector.connect(
+    host = '127.0.0.1',
+    port = '3306',
+    user = 'root',
+    password = 'Root00root',
+    database = 'alx_book_store',
+)
 
 #* EXECUTE COMMAND TO DB
 if mydb.is_connected():
